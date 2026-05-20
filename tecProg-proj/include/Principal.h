b178c83308a75f92b815bddef81192f194e09955
brunno <brunnomurilo1@gmail.com>
2026-05-20 01:54:22 -0300

    Adicao class Player

--- tecProg-proj/include/Principal.h
+++ tecProg-proj/include/Principal.h
@@ -2,11 +2,14 @@
 #define PRINCIPALH
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <thread>
+#include "Player.h"
 
 namespace Alaska {
 	class Principal {
 	private:
 		sf::RenderWindow window;
+		Player::Player player;
 	public:
 		Principal();
 		~Principal();
---
+++ tecProg-proj/src/Player.cpp
@@ -0,0 +1,35 @@
+#include "../include/Player.h"
+
+using namespace Alaska::Player;
+
+Player::Player(const sf::Vector2f pos, const sf::Vector2f size): body(sf::Vector2f(size)), vel(sf::Vector2f(0.1f, 0.1f)) {
+	body.setPosition(pos);
+	body.setFillColor(sf::Color::Green);
+	createPlayer();
+}
+Player::Player(const sf::RectangleShape body): body(body), vel(sf::Vector2f(0.1f, 0.1f)) {
+	createPlayer();
+}
+Player::Player(): body() {
+	createPlayer();
+}
+Player::~Player() {
+}
+void Player::createPlayer() {
+	vel = sf::Vector2f(0.1f, 0.1f);
+	body.setFillColor(sf::Color::Cyan);
+}
+const sf::RectangleShape Player::getBody() const {
+	return body;
+}
+void Player::move() {
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+		body.move(-vel.x, 0.0f);
+	}else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+		body.move(vel.x, 0.0f);
+	}else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+		body.move(0.0f, -vel.y);
+	}else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+		body.move(0.0f, vel.y);
+	}
+}
